e9148ac08059d5c31bbc00fe2cfd4f7e44b74a50
Yohan9H <yhurtebize58@gmail.com>
2024-11-07 17:23:44 +0100

    init ex02 for week

--- CPP-01/ex00/main.cpp
+++ CPP-01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yohurteb <yohurteb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 07:52:01 by yohurteb          #+#    #+#             */
-/*   Updated: 2024/11/07 08:40:18 by yohurteb         ###   ########.fr       */
+/*   Updated: 2024/11/07 08:43:23 by yohurteb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,10 @@
 int	main()
 {
 	Zombie	*p_zombie = newZombie("Fred");
-	p_zombie->announce();
 
 	randomChump("Jack");
+	randomChump("Paul");
+	p_zombie->announce();
+	randomChump("Jean");
 	delete p_zombie;
 }
