31f968e70bc2f340ebe50c4b70146a64de651743
Yohan9H <yhurtebize58@gmail.com>
2024-11-07 08:08:11 +0100

    init ex00

---
+++ CPP-01/ex00/Zombie.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yohurteb <yohurteb@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 07:52:11 by yohurteb          #+#    #+#             */
+/*   Updated: 2024/11/07 08:01:33 by yohurteb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+# include <iostream>
+
+class Zombie
+{
+	public:
+		Zombie(){
+
+		};
+		void	announce(void);
+
+	private:
+		std::string	name;
+};
+
+#endif
