f522563d8989c56dadac18e9a03764bc47a6facb
Yohan9H <yhurtebize58@gmail.com>
2024-11-15 11:28:02 +0100

    modif

--- CPP-01/ex01/zombieHorde.cpp
+++ CPP-01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: yohurteb <yohurteb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 09:36:55 by yohurteb          #+#    #+#             */
-/*   Updated: 2024/11/07 09:51:41 by yohurteb         ###   ########.fr       */
+/*   Updated: 2024/11/14 11:02:05 by yohurteb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 Zombie*		zombieHorde(int N, std::string name)
 {
 	Zombie* horde = new Zombie[N];
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i <= N; i++)
 		horde[i].setNameZombie(name);
 	return (horde);
 }
