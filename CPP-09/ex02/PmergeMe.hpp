69148887795cbc0a21d0101ec022b54309504467
Yohan9H <yhurtebize58@gmail.com>
2025-02-06 16:08:46 +0100

    begin ex02

---
+++ CPP-09/ex02/PmergeMe.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yohurteb <yohurteb@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/04 11:55:08 by yohurteb          #+#    #+#             */
+/*   Updated: 2025/02/06 15:07:16 by yohurteb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <ctime>
+#include <exception>
+#include <cstdlib>
+
---
+++ CPP-09/ex02/main.cpp
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yohurteb <yohurteb@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/04 11:55:15 by yohurteb          #+#    #+#             */
+/*   Updated: 2025/02/06 16:05:31 by yohurteb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+
+void	FordJohnsonDeq(std::deque<int>::iterator begin, std::deque<int>::iterator end)
+{
+	
+}
+
+void	FordJohnsonVec(std::vector<int>::iterator begin, std::vector<int>::iterator end)
+{
+	
+}
+
+template <typename T1>
+T1	ChangeAvtoArray(char **av)
+{
+	T1 array;
+	int val;
+	char *ptr;
+
+	for (int i = 1; av[i]; i++)
+	{
+		if (!*av[i])
+			throw(1);
+		val = strtol(av[i], &ptr, 10);
+		if (*ptr || val < 0)
+			throw(1);
+		array.push_back(val);
+	}
+	return array;
+}
+
+int	main(int ac, char **av)
+{
+	clock_t timeVec;
+	clock_t	timeDeq;
+
+	if (ac < 2)
+	{
+		std::cerr << "Error: Not enaugh numbers" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	try
+	{
+		
+		std::vector<int> vec = ChangeAvtoArray<std::vector<int> >(av);
+		timeVec = clock();
+		FordJohnsonVec(vec.begin(), vec.end());
+		timeVec = clock() - timeVec;
+
+		std::deque<int> deq = ChangeAvtoArray<std::deque<int> >(av);
+		timeDeq = clock();
+		FordJohnsonDeq(deq.begin(), deq.end());
+		timeDeq = clock() - timeDeq;
+	}
+	catch(...)
+	{
+		std::cerr << "Error" << std::endl;
+	}
+}
